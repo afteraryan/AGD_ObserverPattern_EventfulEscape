91d3c56e6c05d7a7630d4810040ab8712e28c2fd
Aryan Saxena <afteraryan@gmail.com>
2024-03-17 18:58:20 +0530

    Used Actions

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -25,11 +25,11 @@ public class PlayerController
 
         this.playerScriptableObject = playerScriptableObject;
         this.playerScriptableObject.KeysEquipped = 0;
-        LightSwitchView.lightToggled += onLightSwitch;
+        LightSwitchView.lightToggledAction += onLightSwitch;
         playerState = PlayerState.InDark;
     }
 
-    ~PlayerController() => LightSwitchView.lightToggled -= onLightSwitch;
+    ~PlayerController() => LightSwitchView.lightToggledAction -= onLightSwitch;
     public void Interact() => IsInteracted = Input.GetKeyDown(KeyCode.E) ? true : (Input.GetKeyUp(KeyCode.E) ? false : IsInteracted);
 
     public void Jump(Rigidbody playerRigidbody, Transform transform)
