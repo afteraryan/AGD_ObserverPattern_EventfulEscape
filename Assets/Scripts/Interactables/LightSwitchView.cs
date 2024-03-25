8bd133faef382b715987f47683ba930da20e4c69
Aryan Saxena <afteraryan@gmail.com>
2024-03-25 12:12:25 +0530

    Used Event Service for Light Switch Toggle

--- Assets/Scripts/Interactables/LightSwitchView.cs
+++ Assets/Scripts/Interactables/LightSwitchView.cs
@@ -9,13 +9,13 @@ public class LightSwitchView : MonoBehaviour, IInteractable
     private SwitchState currentState;
     public static event Action OnLightSwitchToggled;
 
-    private void OnEnable() => OnLightSwitchToggled += onLightSwitch;
+    private void OnEnable() => EventService.Instance.OnLightSwitchToggled.AddListener(onLightSwitch);
 
-    private void OnDisable() => OnLightSwitchToggled -= onLightSwitch;
+    private void OnDisable() => EventService.Instance.OnLightSwitchToggled.RemoveListener(onLightSwitch);
 
     private void Start() => currentState = SwitchState.Off;
 
-    public void Interact() => OnLightSwitchToggled?.Invoke();
+    public void Interact() => EventService.Instance.OnLightSwitchToggled.InvokeEvent();
 
     private void toggleLights()
     {
